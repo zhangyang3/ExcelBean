deac4f67e5086e121df004b2e2cfa904354a4259
z520zlove@vip.qq.com <yangzhang7@zy-PC>
2018-03-15 15:43:19 +0800

    LengthValidatorProcessor逻辑修改

--- src/main/java/com/imzy/excel/processer/validator/LengthValidateProcessor.java
+++ src/main/java/com/imzy/excel/processer/validator/LengthValidateProcessor.java
@@ -1,6 +1,7 @@
 package com.imzy.excel.processer.validator;
 
 import com.imzy.excel.annotations.ProcessorDescription;
+import com.imzy.excel.processer.ValidateProcessor;
 import com.imzy.excel.util.StringUtils;
 
 /**
@@ -9,23 +10,24 @@ import com.imzy.excel.util.StringUtils;
  *
  */
 @ProcessorDescription(description = "长度校验器")
-public class LengthValidateProcessor extends RegularValidateProcessor {
+public class LengthValidateProcessor implements ValidateProcessor {
 
 	@Override
 	public boolean validate(String value, String[] validatorParam) {
-		String regex = StringUtils.EMPTY;
 
 		if (validatorParam.length == 1) {
-			regex = "^.{" + validatorParam[0] + "}$";
+			return value.length() == Integer.parseInt(validatorParam[0]);
 		} else {
-			if (StringUtils.isBlank(validatorParam[1])) {
-				regex = "^.{" + validatorParam[0] + ",}$";
+			if (StringUtils.isBlank(validatorParam[0]) && StringUtils.isNotBlank(validatorParam[1])) {
+				return value.length() <= Integer.parseInt(validatorParam[1]);
+			} else if (StringUtils.isBlank(validatorParam[1]) && StringUtils.isNotBlank(validatorParam[0])) {
+				return value.length() >= Integer.parseInt(validatorParam[0]);
 			} else {
-				regex = "^.{" + validatorParam[0] + "," + validatorParam[1] + "}$";
+				return value.length() >= Integer.parseInt(validatorParam[0])
+						&& value.length() <= Integer.parseInt(validatorParam[1]);
 			}
 		}
 
-		return super.validate(value, new String[] { regex });
 	}
 
 }
